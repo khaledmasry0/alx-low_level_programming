39d2b4672bc81b827560788eb98bbb213bc4c59b
khaled <khaledmasryms@gmail.com>
2023-04-04 20:39:04 -0700

    commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print str
+ * *@S: str of char
+*/
+
+void _puts_recursion(char *S)
+{
+	if (*S == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
