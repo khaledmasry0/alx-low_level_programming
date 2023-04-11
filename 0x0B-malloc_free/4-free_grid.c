5cdfa9c0e395ea3e208b37a5d04ae612344f7bc3
khaled <khaledmasryms@gmail.com>
2023-04-11 10:02:01 -0700

    commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * free_grid - free the grid
+ * @grid: grid of memories
+ * @height: int
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
