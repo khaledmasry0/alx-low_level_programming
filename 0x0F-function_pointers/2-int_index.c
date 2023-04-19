683f61289a8e6d37e0b7e376e58830333966f60d
khaled <khaledmasryms@gmail.com>
2023-04-19 13:42:00 -0700

    commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for integer
+ * @array: the integer array
+ * @size: the array size
+ * @cmp: the compare function
+ * Return: the integer i
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array && size && cmp)
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+			i++;
+		}
+	return (-1);
+}
