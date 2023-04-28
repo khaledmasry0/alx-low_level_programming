f57b56b669db0972b5e4f4b2b037f67ae14cc317
khaledmasry0 <khaledmohamed3360@gmail.com>
2023-04-27 18:17:00 -0700

    commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - determines l of linked list
+ * @h: pointer to first n
+ * Return: size of list
+*/
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
