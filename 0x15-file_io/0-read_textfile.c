a6a9adbd371d2856479b3b388851e34ea38a3adb
khaledmasry0 <khaledmohamed3360@gmail.com>
2023-05-09 03:02:52 -0700

    commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,17 +8,17 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int ftd
+	int fd
 	ssize_t bytes;
 	char buf[READ_BUF_SIZE * 8];
 
 	if (!filename || !letters)
 		return (0);
-	ftd = open(filename, O_RDONLY);
-	if (ftd == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	bytes = read(ftd, &buf[0], letters);
+	bytes = read(fd, &buf[0], letters);
 	bytes = write(STDOUT_FILENO, &buf[0], bytes);
-	close(ftd);
+	close(fd);
 	return (bytes);
 }
